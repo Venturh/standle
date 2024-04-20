8e1a2bcca10bf0ac7eee04dabd7dea4b9e901e19
Max <33336634+Venturh@users.noreply.github.com>
2024-04-20 23:40:05 +0200

    adjust tauri config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-//@ts-expect-error ok
-import { devtools } from "@vue/devtools";
 import { createApp } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import App from "./App.vue";
 
 import "./assets/main.postcss";
-
-if (process.env.NODE_ENV === "development") {
-  devtools.connect("http://localhost", 8098);
-}
 
 createApp(App).use(VueQueryPlugin).mount("#app");
